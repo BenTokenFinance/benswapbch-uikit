a26d65cf22c495394100ce250206a1f080f59d89
Ben Token Finance <admin@bentoken.finance>
2024-08-12 14:34:47 +0800

    x icon

---
+++ src/widgets/Menu/icons/X.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Svg from "../../../components/Svg/Svg";
+import { SvgProps } from "../../../components/Svg/types";
+
+const Icon: React.FC<SvgProps> = (props) => {
+  return (
+    <Svg viewBox="0 0 1226.37 1226.37" {...props}>
+      <g transform="matrix(0.6599999999999998,0,0,0.6599999999999998,208.4829001426695,155.4828991699219)"><path d="M727.348 519.284 1174.075 0h-105.86L680.322 450.887 370.513 0H13.185l468.492 681.821L13.185 1226.37h105.866l409.625-476.152 327.181 476.152h357.328L727.322 519.284zM582.35 687.828l-47.468-67.894-377.686-540.24H319.8l304.797 435.991 47.468 67.894 396.2 566.721H905.661L582.35 687.854z" /></g>
+    </Svg>
+  );
+};
+
+export default Icon;
